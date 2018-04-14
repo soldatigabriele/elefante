a10ba035ec1872393b948094824da7be1f22a03b
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-04-14 02:33:14 +0100

    update

--- resources/views/fanta/index.blade.php
+++ resources/views/fanta/index.blade.php
@@ -64,59 +64,80 @@
             <br>
         </form>
     </div>
-</div>
 
 @if (session()->has('fantas'))
     
 <div class="clearfix"></div><br>
 
-    <div class="offset-2 col-8">
-    <div class="card carta">
-        <div class="col-12">
-            <h4>Matches: {{ count(session('fantas')[0]) }}</h4>
-        </div>
-        <div class="clearfix"></div><br>
+    <div class="" style="padding:10px;margin:6px;text-align:center;">
+        <h4>Matches</h4>
+        <div class="row">
         @foreach(session('fantas') as $fs)
             @foreach($fs as $fanta)
-            <div class="row">
-                <div class="col-10">
-                    <div class="row">
-                        <div class="col">
-                        @if($fanta->preview)
-                            <img src="/images/{{$fanta->id}}/{{$fanta->preview}}" width="100px">
-                        @endif
-                        </div>
-                        <div class="col-2">
-                            {{ $fanta->flavour->name }}
-                        </div>
-                        <div class="col-2">
-                            {{ $fanta->country->name }}
-                        </div>
-                        <div class="col-1">
-                            {{ $fanta->year }}
-                        </div>
-                        <div class="col">
-                            @foreach($fanta->tags as $tag)
-                                {{ $tag->name }}
-                            @endforeach
-                        </div>
-                        <div class="col">
-                            @foreach($fanta->colours as $colour)
-                                {{ $colour->name }}
-                            @endforeach
-                        </div>
-                    </div>
+@if($fanta->preview)
+            <div class="col" style="max-width:280px; padding:10px">
+                <div class="card">
+                    
+                <div class="card-body" style="text-align:center;margin-bottom:10px;">
+                    <img src="/images/{{$fanta->id}}/{{$fanta->preview}}" width="100px">
+                    <div class="clearfix"></div><br>
+                    <a role="button" class="btn btn-outline-warning btn-sm" href="{{ route('edit-fanta', $fanta) }}">{{ $fanta->flavour->name }}</a>
                 </div>
-                <div class="col-2">
-                    <a role="button" class="btn btn-outline-warning" href="{{ route('edit-fanta', $fanta->id) }}">Edit</a>
                 </div>
             </div>
-            <div class="clearfix"></div><hr>
+@endif
             @endforeach
         @endforeach
+        </div>
     </div>
-</div>
 @endif
+
+@if (session()->has('fantas'))
+
+    <div class="clearfix"></div><hr>
+
+    <h4>Matches with no preview</h4>
+
+            <div class="col-12">
+                <div class="row">
+                @foreach(session('fantas') as $fs)
+                    @foreach($fs as $fanta)
+                        @if(!$fanta->preview)
+                        <div class="col-md-4 col-sm-6" style="margin-bottom: 10px">
+                            <div class="card">
+                                <div class="card-body">
+                                
+                                    <div>
+                                        Colour: 
+                                        @foreach($fanta->colours as $colour)
+                                            {{$colour->name}},
+                                        @endforeach
+                                    </div>
+
+                                    <div>
+                                        Flavour: {{ $fanta->flavour->name }}
+                                    </div>
+                                    <div>
+                                        Tags: 
+                                        @foreach($fanta->tags as $tag)
+                                            {{$tag->name}},
+                                        @endforeach
+                                    </div>
+                                    <div>
+                                        Year: {{$fanta->year}}
+                                    </div>
+                                    <a role="button" class="btn btn-outline-warning btn-sm" href="{{ route('edit-fanta', $fanta) }}">Open</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endforeach
+                @endforeach
+                </div>
+            </div>
+@endif
+
+</div>
 
 @endsection
 
