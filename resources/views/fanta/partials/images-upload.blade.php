6591064349fe9cb48e1ae46bf8a62ddbfc15be59
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-04-15 14:17:08 +0100

    update

--- resources/views/fanta/partials/images-upload.blade.php
+++ resources/views/fanta/partials/images-upload.blade.php
@@ -29,7 +29,7 @@
     <h4>Preview</h4>
     <div class="card" style="width:300px;text-align: center;padding:10px;">
         <div style="">
-            <img src="/images/{{$fanta->id}}/{{$fanta->preview}}" width="200px">
+            <img src="{{ asset('storage/images/'.$fanta->id.'/'.$fanta->preview) }}" width="200px">
             <div class="clearfix"></div><br>
             <a role="button" class="btn btn-outline-danger btn-sm" href="{{ route('preview.destroy', $fanta) }}"> Delete preview</a>
         </div>
@@ -41,11 +41,12 @@
         <h4>Sides</h4>
         <div class="row">
         @foreach($fanta->images as $image)
+        
         <div class="col" style="max-width:280px; padding:10px">
             <div class="card">
                 
             <div class="card-body" style="text-align:center;margin-bottom:10px;">
-                <img src="/images/{{$fanta->id}}/{{$image->normal_size}}" width="200px">
+                <img src="{{ asset('storage/images/'.$fanta->id.'/'.$image->normal_size) }}" width="200px">
                 <div class="clearfix"></div><br>
                 <a role="button" class="btn btn-outline-danger btn-sm" href="{{ route('side.destroy', [$fanta, $image]) }}"> Delete this image</a>
             </div>
