d1e4a6384e5c4ef7172f96972d85b89379daf857
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-04-20 20:17:50 +0100

    update

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+
+// let mix = require('laravel-mix');
+
+// mix
+//     .js('resources/assets/js/app.js', 'public/js')
+//     .js('node_modules/selectize/src/selectize.js', 'public/js/application.js')
+//     .sass('resources/assets/sass/app.scss', 'public/css');
+
+// mix.browserSync('http://elefante.test',{
+//     open: false
+//  });
+
+//  mix.options({ processCssUrls: false, uglify: false });
+//  mix.disableNotifications();
