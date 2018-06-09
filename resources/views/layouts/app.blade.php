f5659c7e6d789487a5805cced5f0857068e6fa6c
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-06-09 22:20:50 +0100

    added google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56950128-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-56950128-3');
+    </script>
+
     <link rel="shortcut icon" href="{{{ asset('favicon.png') }}}">
 
     <meta charset="utf-8">
--- resources/views/layouts/fanta.blade.php
+++ resources/views/layouts/fanta.blade.php
@@ -1,25 +1,35 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <link rel="shortcut icon" href="{{{ asset('favicon.png') }}}">
-    
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56950128-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-    <title>Elefante</title>
+    gtag('config', 'UA-56950128-3');
+  </script>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/front.css') }}" rel="stylesheet" type="text/css"   >
-    <link href="/css/dropzone.css" rel="stylesheet" type="text/css"   >
-    
-    <!-- Scripts -->
-    <script src="/js/application.js"></script>
-    <script src="/js/dropzone.js"></script>
-    <script src="/js/charts.js"></script>
-    
+  <link rel="shortcut icon" href="{{{ asset('favicon.png') }}}">
+  
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Elefante</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/front.css') }}" rel="stylesheet" type="text/css"   >
+  <link href="/css/dropzone.css" rel="stylesheet" type="text/css"   >
+  
+  <!-- Scripts -->
+  <script src="/js/application.js"></script>
+  <script src="/js/dropzone.js"></script>
+  <script src="/js/charts.js"></script>
+  
   @yield('header-scripts')
     <style>
     .selectize-input>.item{
@@ -33,7 +43,7 @@
     }
     </style>
   @yield('style')
-<meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
   
