c8d49663825eb628a233b2197fee02969d00baf1
Gabriele Soldati <soldati.gabriele@gmail.com>
2021-01-17 13:45:09 +0000

    add year to api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Tag;
+use App\Fanta;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +16,20 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::get('/tags', function(Request $request){
-    Log::channel('api')->info(['ip'=>$request->ip(), 'request'=>$request->toArray()]);
+Route::get('/tags', function (Request $request) {
+    Log::channel('api')->info(['ip' => $request->ip(), 'request' => $request->toArray()]);
     return Tag::inRandomOrder()->take($request->number ?? 1)->pluck('name');
+});
+
+Route::get('/year/{year}', function (Request $request, $year) {
+    Log::channel('api')->info(['ip' => $request->ip(), 'request' => $request->toArray()]);
+    $fanta = Fanta::where('year', $year)->inRandomOrder()->first() ?? Fanta::inRandomOrder()->first();
+    return JsonResponse::create([
+        'country' => $fanta->country->name,
+        'colour' => $fanta->getColours(),
+    ]);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
